92e76bb2261a99b19ba8c8eb0b44aa7af50bf8e9
Andrey <d48564@gmail.com>
2019-08-30 19:03:59 +0300

    Updates

---
+++ precache-manifest.da48452d7b8e3486a14231561f83b7bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8346f08ad807b09bdabca3bb7803a8ec",
+    "url": "/React-star-wars/index.html"
+  },
+  {
+    "revision": "83bd69832936438f1e7a",
+    "url": "/React-star-wars/static/css/main.cea24633.chunk.css"
+  },
+  {
+    "revision": "7da14ca68fb870f305ef",
+    "url": "/React-star-wars/static/js/2.29cecfc7.chunk.js"
+  },
+  {
+    "revision": "83bd69832936438f1e7a",
+    "url": "/React-star-wars/static/js/main.dc3d8c87.chunk.js"
+  },
+  {
+    "revision": "88b770067367a174516c",
+    "url": "/React-star-wars/static/js/runtime~main.85cec4ef.js"
+  }
+]);
